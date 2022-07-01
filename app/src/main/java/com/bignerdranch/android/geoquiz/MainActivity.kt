450ce8119ae8bcd0ad66f55a3d1cadb2a50cfeca
Ali <Sawahreh-ali@hotmail.com>
2022-07-01 22:17:56 +0300

    Add ImageButtons

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
     private val questionBank: List<Question> = listOf(
             Question(R.string.question_australia, true),
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View->
             checkAnswer(true)
@@ -35,6 +38,16 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         nextButton.setOnClickListener {
+            view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        previousButton.setOnClickListener {
+            view: View ->
+            currentIndex = (currentIndex -1) % questionBank.size
+            updateQuestion()
+        }
+        questionTextView.setOnClickListener {
             view: View ->
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
