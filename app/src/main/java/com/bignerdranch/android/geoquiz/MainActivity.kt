9683d3c5cc5648c3ee58492783419d1b0495e503
Ali <Sawahreh-ali@hotmail.com>
2022-07-01 21:46:36 +0300

    Add next button

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -29,10 +29,10 @@ class MainActivity : AppCompatActivity() {
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View->
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
         }
         nextButton.setOnClickListener {
             view: View ->
@@ -44,5 +44,12 @@ class MainActivity : AppCompatActivity() {
     private fun  updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if(userAnswer == correctAnswer) R.string.correct_toast
+                           else R.string.incorrect_toast
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
